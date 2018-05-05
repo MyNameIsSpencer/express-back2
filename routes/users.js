508eb6885f1579df45b2b7232aa135792e063874
spencer kyonka <spencer.kyonka@hotmail.ca>
2018-05-05 18:32:12 -0400

    express users working

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,10 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  res.json([
+    {id: 1, username: 'thisguy'},
+    {id: 2, username: 'otherguy'}
+  ]);
 });
 
 module.exports = router;
